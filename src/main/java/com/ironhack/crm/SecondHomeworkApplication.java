3634a845d28246abf9ba60f82aefa088e8df0312
pauGuarch <guarch.pau@gmail.com>
2022-08-13 13:57:57 +0200

    refactor package name

--- src/main/java/com/ironhack/crm/SecondHomeworkApplication.java
+++ src/main/java/com/ironhack/crm/SecondHomeworkApplication.java
@@ -1,4 +1,4 @@
-package com.ironhack.demo;
+package com.ironhack.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ironhack/crm/SecondHomeworkApplicationTests.java
+++ src/test/java/com/ironhack/crm/SecondHomeworkApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ironhack.demo;
+package com.ironhack.crm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
