4d0f32dc1132a8fe2ac6b6ec167b1ffcd05f868d
pauGuarch <guarch.pau@gmail.com>
2022-08-13 09:36:36 +0200

    project structure definition

---
+++ src/main/java/com/ironhack/demo/SecondHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondHomeworkApplication.class, args);
+	}
+
+}
