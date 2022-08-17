5efee217943206d2b10cc5ccfeefcdf042a7a03e
pauGuarch <103140593+pauGuarch@users.noreply.github.com>
2022-08-17 20:08:04 +0200

    Oportunity#9 (#3)
    * Oportunity class and status Enum creation
    
    * close method definition
    
    * Refactor oportunity
    
    Co-authored-by: CarlosOrbegozo <43339018+CarlosOrbegozo@users.noreply.github.com>

--- src/main/java/com/ironhack/crm/models/Oportunity.java
+++ src/main/java/com/ironhack/crm/models/Oportunity.java
@@ -9,12 +9,29 @@ public class Oportunity {
     private Contact decisionMaker;
     private int quantity;
     private OportunityStatus status;
+    private String product;
+    private int quantity;
+    private Company company;
+
+    public Oportunity(UUID id, Contact decisionMaker, int quantity, OportunityStatus status, String product, int quantity1, Company company) {
+
 
     public Oportunity(UUID id, Contact decisionMaker, int quantity, OportunityStatus status) {
         this.id = id;
         this.decisionMaker = decisionMaker;
         this.quantity = quantity;
         this.status = status;
+        this.product = product;
+        this.quantity = quantity1;
+        this.company = company;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
     }
 
     public Contact getDecisionMaker() {
@@ -41,6 +58,29 @@ public class Oportunity {
         this.status = status;
     }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public boolean close(OportunityStatus status){
+        if (status == OportunityStatus.CLOSED_LOST || status == OportunityStatus.CLOSED_WON){
+            setStatus(status);
+            return true;
+        }else{
+            return false;
+        }
     public boolean close(){
         return true;
     }
