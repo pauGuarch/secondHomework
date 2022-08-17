5369c364cbe056931dc0a28dced1be8a51b4bd21
pauGuarch <103140593+pauGuarch@users.noreply.github.com>
2022-08-17 20:02:54 +0200

    create Lookup method (#4)

---
+++ src/main/java/com/ironhack/crm/utils/Utils.java
@@ -0,0 +1,10 @@
+package com.ironhack.crm.utils;
+
+import com.ironhack.crm.models.Oportunity;
+
+public class Utils {
+    public static void lookUpOportunity(Oportunity oportunity){
+        System.out.println(oportunity.toString());
+    }
+
+}
