b2775ef2571162364237eec06a17bf0f90e9f725
pauGuarch <guarch.pau@gmail.com>
2022-08-21 18:20:47 +0200

    user inputs for lead object

--- src/main/java/com/ironhack/crm/SecondHomeworkApplication.java
+++ src/main/java/com/ironhack/crm/SecondHomeworkApplication.java
@@ -2,21 +2,57 @@ package com.ironhack.crm;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.ironhack.crm.dao.manager.implementation.AccountManagerImpl;
+import com.ironhack.crm.domain.enums.OpportunityStatus;
+import com.ironhack.crm.domain.enums.ProductType;
+import com.ironhack.crm.domain.models.Account;
+import com.ironhack.crm.domain.models.Contact;
 import com.ironhack.crm.domain.models.Opportunity;
+import com.ironhack.crm.domain.models.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class SecondHomeworkApplication {
 
-	public static void main(String[] args) {
-		System.out.println(readOpportunities().get(1).getQuantity());
+	public static void main(String[] args) throws IOException {
+		//System.out.println(readOpportunities().get(1).getQuantity());
 		//SpringApplication.run(SecondHomeworkApplication.class, args);
+
+		AccountManagerImpl acc = new AccountManagerImpl();
+		Contact contact1 = new Contact("Juan", "<@juan", "647733243", "Company1");
+		Contact contact2 = new Contact("Pedro", "<@pedro", "647755543", "Company2");
+		List<Contact> contactList = new ArrayList<Contact>();
+		contactList.add(contact1);
+		contactList.add(contact2);
+
+		Product product = new Product("product1", ProductType.FLATBED);
+		Product product2 = new Product("product2", ProductType.HYBRID);
+
+		Opportunity opportunity1 = new Opportunity(contact1, 3, OpportunityStatus.OPEN, product);
+		Opportunity opportunity2 = new Opportunity(contact2, 6, OpportunityStatus.CLOSED_LOST, product);
+		List<Opportunity> opportunityList = new ArrayList<Opportunity>();
+		opportunityList.add(opportunity1);
+		opportunityList.add(opportunity2);
+
+
+		Account acc1 = new Account("Industry1", 5, "bcn", "spain", contactList, opportunityList);
+		Account acc2 = new Account("Industry2", 10, "bdn", "ravalistan", contactList, opportunityList);
+		List<Account> accountList = new ArrayList<Account>();
+		acc.accounts = new ArrayList<Account>();
+		acc.accounts.add(acc1);
+		acc.accounts.add(acc2);
+
+		acc.writeJSON();
 	}
 
 	private static List<Opportunity> readOpportunities() {
--- src/main/java/com/ironhack/crm/dao/manager/implementation/AccountManagerImpl.java
+++ src/main/java/com/ironhack/crm/dao/manager/implementation/AccountManagerImpl.java
@@ -1,16 +1,20 @@
 package com.ironhack.crm.dao.manager.implementation;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.ironhack.crm.dao.manager.AccountManager;
 import com.ironhack.crm.domain.models.Account;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class AccountManagerImpl implements AccountManager {
-    private List<Account> accounts = checkAccounts();
+    public List<Account> accounts = checkAccounts();
     @Override
     public void createAccount(Account account) {
 
@@ -31,4 +35,5 @@ public class AccountManagerImpl implements AccountManager {
             ex.printStackTrace();
         }
     }
+
 }
